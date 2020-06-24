932e843d255137ae9aedd57bb8bfa2ed43f6d687
ctdupuis <ctdupuis7@gmail.com>
2020-06-23 22:09:50 -0500

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
   render() {
+    const { restaurants, deleteRestaurant } = this.props;
+    const restLis = restaurants.map(res => {
+      return(<Restaurant key={res.id} restaurant={res} deleteRestaurant={deleteRestaurant} />)
+    })
     return(
       <ul>
-        Restaurants Component
+        {restLis}
       </ul>
     );
   }
